a0cf5b6952530cd6f8e21ee50540f80c729821db
Mondal-Prasun <prasun2023work@gmail.com>
2024-04-12 16:50:43 +0530

    intilized firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
 import 'package:ciscord/screens/default_screen.dart';
+import 'package:ciscord/screens/login_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -24,7 +32,7 @@ class MyApp extends StatelessWidget {
           ),
           textTheme: GoogleFonts.tiltNeonTextTheme(),
         ),
-        home: const DefaultScreen(),
+        home: const LoginScreen(),
       ),
     );
   }
