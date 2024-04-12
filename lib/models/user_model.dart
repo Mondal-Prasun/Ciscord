2efa670d17c80ada1d72577facf0e0ddcc997362
Mondal-Prasun <prasun2023work@gmail.com>
2024-04-12 15:26:30 +0530

    added user class

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,3 +1,21 @@
 class User {
-  User();
+  User({required this.userName, required this.email, required this.userImage});
+
+  final String userName;
+  final String email;
+  String _password = "";
+  final String userImage;
+
+  set setPassword(String password) {
+    if (password != null &&
+        password.isNotEmpty &&
+        password.length > 6 &&
+        password.contains(RegExp(r"[0-9]"))) {
+      _password = password;
+    }
+  }
+
+  get getPassword {
+    return _password;
+  }
 }
